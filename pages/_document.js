fc912441238910be2b0945d9df6bda1468338567
Finsen Antonius <finsenantonius42@gmail.com>
2022-04-11 09:41:21 +0700

    fix gtm code

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script dangerouslySetInnerHTML={{ __html: (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MMKC36Z')}}></script>
+        })(window,document,'script','dataLayer','GTM-MMKC36Z');`}}></script>
         <link
           rel="preload"
           href="/fonts/Serca-Regular.otf"
@@ -35,8 +35,8 @@ export default function Document() {
         />
       </Head>
       <body>
-        <noscript dangerouslySetInnerHTML={{ __html: <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MMKC36Z"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe>}}></noscript>
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MMKC36Z"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
         <Main />
         <NextScript />
       </body>
