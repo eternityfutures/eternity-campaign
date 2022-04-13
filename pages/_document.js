923bb9997dd7a131e4a6cae45ac5a08b8bf9d265
Finsen Antonius <finsenantonius42@gmail.com>
2022-04-13 14:27:15 +0700

    add analytic

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,13 @@ export default function Document() {
         gtag('js', new Date());
         gtag('config', '${gtag.GA_TRACKING_ID}');`}}></script>
 
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${gtag.ANALYTIC_ID}`}></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${gtag.ANALYTIC_ID}');`}}></script>
+
         <link
           rel="preload"
           href="/fonts/Serca-Regular.otf"
